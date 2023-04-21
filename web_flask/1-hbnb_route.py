80bfdab72cb4c33377870a162626669fad7f7fa5
Kaegan Davis Haidome <obichi@coseo.com>
2023-04-21 07:46:40 +0100

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ app = Flask('__name__')
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """Return a given string"""
     return ("Hello HBNB")
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Returns a given string"""
     return ("HBNB")
 
 if __name__ == '__main__':
