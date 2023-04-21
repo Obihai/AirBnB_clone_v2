85df3f6ba16ac0896d567ac33c225653acb498b8
Kaegan Davis Haidome <obichi@coseo.com>
2023-04-21 08:07:43 +0100

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-""" 
-Write a script that starts a Flask web application:
+""" Write a script that starts a Flask web application:
 Your web application must be listening on 0.0.0.0, port 5000
 """
 
 from flask import Flask
 
-app = Flask('__name__')
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Return a given string"""
-    return 'Hello HBNB'
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Return a given string"""
-    return 'HBNB'
+    """Returns a given string"""
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
