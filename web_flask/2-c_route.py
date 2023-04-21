a178acbe4553bb5b3d3b7e3f39b5e764ee3479d4
Kaegan Davis Haidome <obichi@coseo.com>
2023-04-21 08:06:40 +0100

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,17 +10,20 @@ app = Flask('__name__')
 
 
 @app.route('/', strict_slashes=False)
-def hello_HBNB():
+def hello_hbnb():
+    """Return a given string"""
     return 'Hello HBNB'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Return a given string"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c(text):
+    """display C followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
 
