cd4e17c2e1ca8ff99ba7465a36492e09ed580ac3
Kaegan Davis Haidome <obichi@coseo.com>
2023-04-21 08:47:54 +0100

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,13 +6,13 @@ Your web application must be listening on 0.0.0.0, port 5000
 
 from flask import Flask
 
-app = Flask('__name__')
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Return a given string"""
-    return ("Hello HBNB")
+    return ("Hello HBNB!")
 
 
 @app.route('/hbnb', strict_slashes=False)
