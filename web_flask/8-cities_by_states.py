e7b35f32b5f177c8b9f953f5270bd0a5caeed594
Kaegan Davis Haidome <obichi@coseo.com>
2023-04-23 18:40:30 +0100

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-"""This script starts a Flask web application"""
-
-
-from flask import Flask, render_template
-from models.state import State
-from models.city import City
+""" Starts a Flash Web Application """
 from models import storage
+from models.state import State
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def shutdown_session(exception=None):
-    """ This function closes the session"""
+def appcontext_teardown(self):
+    """use storage for fetching data from the storage engine
+    """
     storage.close()
 
 
-@app.route('/cities_by_states')
-def show_states():
-    """ This function renders the template 8-cities_by_states.html"""
-    states = storage.all(State).values()
-    cities = storage.all(City).values()
+@app.route('/cities_by_states', strict_slashes=False)
+def citiesByStates():
+    """Display a HTML page inside the tag BODY"""
     return render_template('8-cities_by_states.html',
-                           states=states, cities=cities)
+                           states=storage.all(State))
 
 
 if __name__ == '__main__':
